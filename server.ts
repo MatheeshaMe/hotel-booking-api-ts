5b5c68e044147af6497292f0092e19881f7d3425
PraveenMatheesha <77117024+Praveen-47@users.noreply.github.com>
2022-11-03 18:39:58 +0530

    server : dev

--- server.ts
+++ server.ts
@@ -1,1 +1,28 @@
-console.log("server");
+import express, { Request, Response } from "express";
+import * as dotenv from "dotenv";
+
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const enviroment: string = process.env.enviroment!;
+const portv: number = +process.env.PORT!;
+const port:number = enviroment === "development" ? portv : 5000;
+const DBURI:string = process.env.MONGO_URL!;
+
+const app = express();
+
+// var __importDefault = (this && __importDefault) || function (mod) {
+//     return (mod && mod.__esModule) ? mod : { "default": mod };
+// };
+// const express_1 = __importDefault(require("express"));
+// const app = express_1.default();
+
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`app is running on ${port}`);
+  mongoose.connect(DBURI, () => {
+    console.log(`db connected 😀`);
+  });
+});
