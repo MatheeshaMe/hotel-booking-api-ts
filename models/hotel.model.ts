b27fac7bb88bae6446bbc648803eeea642470fa0
PraveenMatheesha <77117024+Praveen-47@users.noreply.github.com>
2022-11-03 18:40:29 +0530

    hotel model

---
+++ models/hotel.model.ts
@@ -0,0 +1,59 @@
+import mongoose from "mongoose";
+import { HotelDocument } from "../interfaces/hotels/IHotels.dto";
+
+const HotelSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  distance: {
+    type: String,
+    required: true,
+  },
+  photos: {
+    type: [String],
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  desc: {
+    type: String,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    min: 0,
+    max: 5,
+  },
+  rooms: {
+    type: [String],
+  },
+  cheapestPrice: {
+    type: Number,
+    required: true,
+  },
+  featured: {
+    type: Boolean,
+    default: false,
+  },
+  weLoved:{
+    type:Boolean,
+    default:false
+  }
+});
+
+const HotelModel = mongoose.model<HotelDocument>("Hotel", HotelSchema);
+export default HotelModel;
