c06b6d956499e8661e4e14ee68e0577aa3b81653
PraveenMatheesha <77117024+Praveen-47@users.noreply.github.com>
2022-11-03 20:54:55 +0530

    hotel middle

--- server.ts
+++ server.ts
@@ -1,23 +1,24 @@
-import express, { Request, Response } from "express";
+import express, { Application, Request, Response, Router } from "express";
 import * as dotenv from "dotenv";
-
+import cors from "cors";
 import mongoose from "mongoose";
+import hotelRouter from "./routes/hotel.routes";
 
 dotenv.config();
 
 const enviroment: string = process.env.enviroment!;
 const portv: number = +process.env.PORT!;
-const port:number = enviroment === "development" ? portv : 5000;
-const DBURI:string = process.env.MONGO_URL!;
+const port: number = enviroment === "development" ? portv : 5000;
+const DBURI: string = process.env.MONGO_URL!;
 
-const app = express();
+const app: Application = express();
 
 // var __importDefault = (this && __importDefault) || function (mod) {
 //     return (mod && mod.__esModule) ? mod : { "default": mod };
 // };
 // const express_1 = __importDefault(require("express"));
 // const app = express_1.default();
-
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 app.listen(port, () => {
@@ -26,3 +27,5 @@ app.listen(port, () => {
     console.log(`db connected 😀`);
   });
 });
+
+app.use("/api/v1/hotels", hotelRouter);
